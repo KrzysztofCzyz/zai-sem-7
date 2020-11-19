f5fb42d52923c64e7be2141bd44533f6f2ee40fc
Krzysztof Czyz <czyz.krzysztof98@gmail.com>
2020-11-19 17:11:04 +0100

    Lab 2 start

--- src/zad6/zad6.ts
+++
@@ -1,4 +0,0 @@
-import {AutorFetcher} from "./autorFetcher";
-
-let x = new AutorFetcher()
-x.execute("dane.json")
--- src/zad7/IPublisher.ts
+++
@@ -1,5 +0,0 @@
-import {Autor} from "./autor";
-
-export interface IPublisher {
-    publish(data: Autor[]): void;
-}
